02919b38c1e72c06e1754482ae576ce5ad306f73
swaraj89 <panigrahiswaraj89@gmail.com>
2023-07-09 12:18:20 +0530

    add a work in progress page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="px-8 my-5 font-thin text-blue-400">Show me the date</h1>
+    <main className="flex min-h-screen flex-col items-center justify-around p-24">
+      <h1 className="px-8 my-5 font-thin from-slate-300">Show me the date</h1>
+      <h2 className="px-8 my-5 font-thin from-slate-300">Work in Progres</h2>
+      <h4 className="px-8 my-5 font-thin from-slate-300">
+        Contact
+        <Link href={"https://swarajpanigrahi.in"}> swarajpanigrahi.in </Link>
+      </h4>
     </main>
   );
 }
